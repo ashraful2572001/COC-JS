92694337db8cbdd6d502d85f3eefe76b539cf976
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-06-30 09:34:25 +0000

    Varaiable about

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 2343;
+let accountEmail = "ashraful@google.com"
+var accountPassword = "1245"
+accountCity = "Sylhet"
+let accountState;
+
+// accountId = 2;
+
+accountEmail = "as@Google.com";
+accountPassword = "432@#"
+accountCity = "Gazipur"
+
+
+// console.log(accountId);
+/*
+Please please not to use var
+bacause of issue in bloack scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
