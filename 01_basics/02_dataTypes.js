6c31cc6cda40d8d351318f64e6370c89790e2ce6
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-06-30 10:07:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+// "use Strict"// treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+console.log(3+3) 
+
+// console.log("ashraful")
+
+
+let name = "Ashraful"
+let age = 24;
+let isLogIn = true;//false
+
+
+// number => 2 to power of 52
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "ashraful")
+console.log(typeof undefined)// undefined
+console.log(typeof null)//object
