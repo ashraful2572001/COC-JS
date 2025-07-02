1b4a5f16d1e720a80337e749fce8da89f54d71bd
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-02 07:38:53 +0000

    Convertion of datatyps

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = false; 
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN/Not a Number
+// undefined = > 0
+// true => 1
+// false = > 0
+
+
+let isLogedIn = "true";
+
+let booleanIsLoggedIn = Boolean(isLogedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitase" => true
+
+
+let someNumber = true;
+
+let stringNumber = String(someNumber)
+console.log(typeof someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
