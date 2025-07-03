6e62cdd85dba1f40927fb0e91f6b382873aa8493
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-03 10:30:47 +0000

    String Datatyps

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+// console.log("hello world")
+const name = "ashra"
+const repoCount = 10;
+
+
+console.log(name + repoCount + " value")
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gamename = new String('ashraful')
+
+
+// console.log(gamename[0])
+// console.log(gamename.__proto__)
+
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(3))
+console.log(gamename.toString())
+
