2f3b49dcf2a0c2e74f5b3d35cc61e88564d3e046
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-10 08:31:51 +0000

    Number Summery

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,25 @@
+// const score = 444;
+
+// console.log(score)
+
+// const balance = new Number(100)
+
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(1))
+
+// const otherNumber = 23.8966;
+
+// console.log(otherNumber.toPrecision(3))
+
+
+// const hundreds = 1000000;
+
+// console.log(hundreds.toLocaleString())
+
+
+
+console.log(Math)
+
