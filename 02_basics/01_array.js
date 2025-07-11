672aac4d3001f26e9db08028749ddb7ad5b6eeca
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-11 10:06:01 +0000

    Array Summery

---
+++ 02_basics/01_array.js
@@ -0,0 +1,51 @@
+// arrays
+
+
+const myArr = [0,1,2,3,4,5, true , "asraful"]
+
+const myHeros = ["Shaktiman", "naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+// console.log(myArr)
+
+// Array methods
+
+
+// myArr.push(6)
+// myArr.push(7)
+
+// myArr.pop()
+
+// myArr.unshift(10)
+
+
+
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf("asraful"))
+
+// const newArray = myArr.join()
+
+// console.log(newArray)
+// console.log(myArr)
+
+
+
+// slice, splice
+
+
+console.log("A", myArr)
+
+const myN1 = myArr.slice(1,3)
+
+console.log(myN1)
+
+console.log("B", myArr)
+
+const myN2 = myArr.splice(1,3)
+
+console.log(myN2)
+
+console.log("C", myArr)
