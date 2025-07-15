dada29589373287c11bbece9bdcffaa7d3c5c833
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-15 08:45:08 +0000

    object summery part 2 complete

---
+++ 02_basics/04_object.js
@@ -0,0 +1,75 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+
+tinderUser.id = "124abck"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false;
+
+
+console.log(tinderUser)
+
+// console.log(tinderUser)
+
+
+
+// const retularUser = {
+//     email: "some@gmail.com",
+//     fullName :{
+//         userFullName: {
+//             fistName : "ashraful",
+//             lastname : "islam"
+//         }
+//     }
+// }
+
+
+// console.log(retularUser.fullName.userFullName.fistName)
+
+// const obj1 = {
+//     1:"a",
+//     2: "b",
+// }
+
+// const obj2 = {
+//     3:"a",
+//     4: "b",
+// }
+
+
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// console.log(obj1)
+
+// const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3)
+
+
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "ashra@gmail.com",
+
+//     }, {
+//         id: 1,
+//         email: "ashra@gmail.com",
+
+//     }, {
+//         id: 1,
+//         email: "ashra@gmail.com",
+
+//     },
+// ]
+
+
+// users[1].email
+
+
+
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isloagged'))
