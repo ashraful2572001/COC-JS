86bc75eba012186c9705470f1016d57d47957ba4
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-15 09:49:18 +0000

    function summery

---
+++ 03_basics/01_function.js
@@ -0,0 +1,31 @@
+
+
+
+// function addTwoNumbers(number1,number2){
+
+
+    // let result = number1 + number2
+    // return result
+
+//     return number1 + number2
+// }
+
+
+// const result = addTwoNumbers(3,5)
+
+// console.log(result)
+
+
+
+function loginUserMessage (username){
+    if(username !== Number){
+        console.log("Please ener a user name")
+        return
+        
+    }
+    // return `${username} just logged in`
+}
+
+
+// console.log(loginUserMessage("Ashraful"))
+console.log(loginUserMessage())
