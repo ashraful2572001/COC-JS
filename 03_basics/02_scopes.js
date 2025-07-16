230eab2935e06779a0a4f13af8ec05476a2a0e45
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-16 08:55:29 +0000

    scope summerty 1st part

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+
+
+let a = 300;
+
+
+if(true){
+
+
+    let a = 10;
+    const b = 20;
+    console.log("INNER:", a)
+
+    function addnum(){
+        
+    }
+}
+
+
+
+
+// console.log(a,b,c)
+
+console.log(a)
