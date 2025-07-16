5aad55572a2a612e01359422b8b6a0a133da489f
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-16 08:41:05 +0000

    econd time function summery

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -17,15 +17,120 @@
 
 
 
-function loginUserMessage (username){
-    if(username !== Number){
-        console.log("Please ener a user name")
-        return
+// function loginUserMessage (username){
+//     if(username !== Number){
+//         console.log("Please ener a user name")
+//         return
         
-    }
+//     }
     // return `${username} just logged in`
-}
+// }
 
 
 // console.log(loginUserMessage("Ashraful"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+// function calculateCartPrice (val1, val2,...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+
+
+
+
+// function handleObject (anyObject){
+//     console.log(`username is ${anyObject.userName} and price is ${anyObject.price}`)
+// }
+
+
+// const user = {
+//     userName: "Ashraful",
+//     price: 199
+// }
+
+// handleObject(user)
+
+
+
+// function calculateCartPrice(val1, val2,...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,400,500,3000,10000,20000))
+
+
+// const user = {
+//     username: "Ashraful",
+//     prices:199,
+// }
+
+
+// function handleObject(PussValue){
+//     console.log(`The use name is ${PussValue.username} and price is ${PussValue.price}`)
+// }
+
+// handleObject(user)
+// handleObject({
+//     username: "Ashraful",
+//     price:199,
+// })
+
+
+
+
+// const myNewArray = [200,400,100,600]
+
+// function returnSecondValue(getArry){
+//     return getArry[1]
+// }
+
+
+// console.log(returnSecondValue(myNewArray))
+// console.log(returnSecondValue([200,400,100]))
+
+
+function calculateCatPrice(val1,val2,...num1){
+    return num1
+}
+
+
+
+console.log(calculateCatPrice(200,4000,500))
+
+
+const use = {
+    username: "ashraful",
+    price: 199,
+}
+
+
+
+// function handelObjec(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+// }
+
+
+// handelObjec(use)
+
+// handelObjec({
+//     username: "same",
+//     price: 399,
+// })
+
+
+
+
+const myNewArrya = [200,400,100,600]
+
+
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+
+// console.log(returnSecondValue(myNewArrya))
+console.log(returnSecondValue([500,200,300,100]))
