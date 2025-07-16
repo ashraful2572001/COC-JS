a2b9c8ea3900d956c20c16bcff7ad0a54bddf6e6
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-16 09:18:54 +0000

    hoisting revition

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,23 +1,74 @@
 
 
-let a = 300;
+// let a = 300;
 
 
-if(true){
+// if(true){
 
 
-    let a = 10;
-    const b = 20;
-    console.log("INNER:", a)
+//     let a = 10;
+//     const b = 20;
+//     console.log("INNER:", a)
 
-    function addnum(){
-        
-    }
-}
+//     function addnum(){
+
+//     }
+// }
 
 
 
 
 // console.log(a,b,c)
 
-console.log(a)
+// console.log(a)
+
+
+
+
+function one(){
+    const userName = "ashraful";
+
+    function two(){
+        const website = "youtube";
+        // console.log(userName)
+    }
+
+    // console.log(website)
+
+    two()
+}
+
+
+// one()
+
+
+
+if (true){
+    const userName = "ashraful";
+
+    if(userName === "ashraful"){
+        const website = " youtube"
+        // console.log(userName + website)
+    }
+
+    // console.log(website)
+}
+
+// console.log(userName)
+
+
+// +++++++++++++++++++++++++++++++ Intersting ++++++++++++++++
+addOne(5)
+
+function addOne (num){
+    return num+1
+}
+
+
+
+
+// addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
