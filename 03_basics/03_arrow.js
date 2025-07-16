fdaa389cb901e049ad4b68afddbecaa1df203c1f
ashraful2572001 <146365295+ashraful2572001@users.noreply.github.com>
2025-07-16 09:51:09 +0000

    arrow function summerty

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,76 @@
+// const user = {
+//     usserName : "Ashraful",
+//     price: 999,
+
+//     welcomemessage: function(){
+//         console.log(`${this.usserName} , welcome to website`)
+//         console.log(this)
+//     }
+
+// }
+
+
+// user.welcomemessage()
+
+// user.usserName = "sam"
+
+// user.welcomemessage()
+
+
+// console.log(this)
+
+
+
+// function chai (){
+//     let username = "ashraful";
+//     console.log(this.username)
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let username = "ashraful";
+//     console.log(this.username)
+// }
+
+// const chai = function(){
+//     let username = "ashraful";
+    // console.log(this.username)
+//     console.log(this)
+
+// }
+// chai()
+
+
+
+
+// const addTw0 = (num1, num2)=>{
+//     return num1 + num2
+
+// }
+
+
+// const addTw0 = (num1, num2)=> num1 + num2
+
+
+// const addTw0 = (num1, num2)=> (num1 + num2)
+
+const addTw0 = (num1, num2)=> ({usernaem: "ashraful"})
+
+
+
+
+
+
+
+console.log(addTw0(3,4))
+
+
+
+const myArray = [2,3,4,5,56,43]
+
+
+myArray.forEach(()=>{
+    
+})
